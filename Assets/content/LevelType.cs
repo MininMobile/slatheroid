633bd47ac8d2e0254adab288695e8c826459b438
MininMobile <miningamer@gmail.com>
2018-04-22 17:02:51 +0100

    Create LevelType Class
    - Remove "spawnPoint" field from Level JSONs

---
+++ Assets/content/LevelType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelType : MonoBehaviour {
+    public int width;
+    public int height;
+    public object[][] level;
+}
