3bcedb84a39fa8d8f3ca49601f5d8fdc5cf609d0
MininMobile <miningamer@gmail.com>
2018-04-22 16:45:19 +0100

    Create Block Prefab
    - Create leveler Script

---
+++ Assets/content/leveler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leveler : MonoBehaviour {
+    public GameObject block;
+    public string level;
+
+	void Start() {
+        if (block == null) throw new System.NullReferenceException("Didn't specify wall prefab for leveler.cs");
+        if (level == null) throw new System.NullReferenceException("Didn't specify json level for leveler.cs");
+
+        LoadLevel(level);
+	}
+    
+    void LoadLevel(string level) {
+        // do shit to load a level
+    }
+}
