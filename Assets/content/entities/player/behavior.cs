38ae08d23dd3825d9de72d4f56f9e96f6889a122
MininMobile <miningamer@gmail.com>
2018-04-23 21:26:00 +0100

    Add Gun Control to Sprite Player

--- Assets/content/entities/player/behavior.cs
+++ Assets/content/entities/player/behavior.cs
@@ -3,23 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class behavior : MonoBehaviour {
-    public Camera mainCamera;
     public GameObject player;
-    public GameObject bullet;
-    public float delay = 0.33f;
     public float speed = 0.1f;
-
-    float timestamp;
 
     void Update() {
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         player.transform.SetPositionAndRotation(this.transform.position, Quaternion.AngleAxis(angle+180, Vector3.forward));
-
-        if (Time.time >= timestamp && Input.GetMouseButton(0)) {
-            Instantiate(bullet, transform.position, transform.rotation);
-            timestamp = Time.time + delay;
-        }
     }
 
     void FixedUpdate() {
---
+++ Assets/content/entities/player/gun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+    public GameObject bullet;
+    public float delay = 0.33f;
+
+    float timestamp;
+
+    void Update() {
+        if (Time.time >= timestamp && Input.GetMouseButton(0)) {
+            Instantiate(bullet, transform.position, transform.rotation);
+            timestamp = Time.time + delay;
+        }
+    }
+}
