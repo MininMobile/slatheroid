adc8569f8fcf2c0146d407b13ef0f1b3bc40016d
MininMobile <miningamer@gmail.com>
2018-04-23 20:05:33 +0100

    Add Bullet Firing Mech

--- Assets/content/entities/player/behavior.cs
+++ Assets/content/entities/player/behavior.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class behavior : MonoBehaviour {
+    public Camera mainCamera;
+    public GameObject bullet;
+    public float delay = 0.33f;
     public float speed = 0.1f;
+
+    float timestamp;
+
+    void Update() {
+        if (Time.time >= timestamp && Input.GetMouseButton(0)) {
+            Instantiate(bullet, transform.position, transform.rotation);
+            timestamp = Time.time + delay;
+        }
+    }
 
     void FixedUpdate() {
        this.transform.Translate(
@@ -11,5 +23,5 @@ public class behavior : MonoBehaviour {
             Input.GetAxis("Vertical")*speed,
             0
             );
-	}
+    }
 }
