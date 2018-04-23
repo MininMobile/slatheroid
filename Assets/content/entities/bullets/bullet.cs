fdd71f4e7f4cfbb4beadf23113c20b4601581b35
MininMobile <miningamer@gmail.com>
2018-04-23 22:34:13 +0100

    Create Bullet Prefab

---
+++ Assets/content/entities/bullets/bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    public float distance = 5f;
+    public float drag = 0.5f;
+
+	void FixedUpdate() {
+        this.transform.Translate(0, distance, 0);
+        distance -= drag;
+
+        if (distance <= 0) Destroy(this);
+	}
+}
